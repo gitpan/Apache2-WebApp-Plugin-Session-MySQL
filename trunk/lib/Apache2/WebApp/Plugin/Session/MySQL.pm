e5d38158109ec9d430bfec2d0035ccef390d5eb5
Marc S. Brooks <mbrooks@cpan.org>
2010-01-02 07:34:54 +0000

    Import of MBROOKS/Apache2-WebApp-Plugin-Session-MySQL-0.06 from CPAN.
    gitpan-cpan-distribution: Apache2-WebApp-Plugin-Session-MySQL
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         MBROOKS/Apache2-WebApp-Plugin-Session-MySQL-0.06.tar.gz
    gitpan-cpan-author:       MBROOKS
    gitpan-cpan-maturity:     released

--- trunk/lib/Apache2/WebApp/Plugin/Session/MySQL.pm
+++ trunk/lib/Apache2/WebApp/Plugin/Session/MySQL.pm
@@ -21,7 +21,7 @@ use Apache::Session::MySQL;
 use Apache::Session::Lock::MySQL;
 use Params::Validate qw( :all );
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~[  OBJECT METHODS  ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
